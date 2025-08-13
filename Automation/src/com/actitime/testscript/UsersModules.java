e04d566299c40241147faf6b9cd397a072db87c5
QSP <QSP@DESKTOP-0JNAIIR>
2025-08-13 14:40:19 +0530

    Create User TestCase

---
+++ Automation/src/com/actitime/testscript/UsersModules.java
@@ -0,0 +1,11 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class UsersModules {
+	@Test
+	public void createUser() {
+	Reporter.log("createUser",true);
+	}
+}
