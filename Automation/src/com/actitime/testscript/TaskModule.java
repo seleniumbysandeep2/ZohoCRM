18b9574a08dba4a5c626a41d00be9c9713ebc8b7
QSP <QSP@DESKTOP-0JNAIIR>
2025-08-14 13:26:53 +0530

    Create Task

---
+++ Automation/src/com/actitime/testscript/TaskModule.java
@@ -0,0 +1,12 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TaskModule {
+	@Test
+	public void createTask() {
+	Reporter.log("createTask",true);
+	}
+	
+}
